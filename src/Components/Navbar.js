c1c1c36ce32fe9608780ca0ad0f393a3a0d3eba9
dongduclan270701 <dongduclan27072001@gmail.com>
2022-09-12 10:06:14 +0700

    first commit

---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {  } from "@mui/icons-material";
+import {  } from "@mui/material";
+const Navbar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Navbar;
---
+++ src/Components/Rightbar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {  } from "@mui/icons-material";
+import {  } from "@mui/material";
+const Rightbar = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Rightbar;
