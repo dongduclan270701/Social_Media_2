d68a4f27b5e6db465f58a99a74bd10d57d7fc27f
dongduclan270701 <dongduclan27072001@gmail.com>
2022-09-12 10:03:38 +0700

    first commit

---
+++ src/Components/Feed.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {  } from "@mui/icons-material";
+import {  } from "@mui/material";
+const Feed = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Feed;
